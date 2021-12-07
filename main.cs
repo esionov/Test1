5559cd66740bf5081b428aae1e6590397ced3898
EdwardSionov <esionov@me.com>
2021-12-07 22:07:40 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+class Program 
+{
+	public static async Task Main (string[] args) 
+  	{
+    	var objects = Enumerable.Range(0, 100);
+		var sum = 0;
+		foreach (var obj in objects)
+		{
+			sum += await ProcessAsync(obj);
+		}
+		Console.WriteLine($"Sum is {sum}");
+  	}
+
+	private static async Task<int> ProcessAsync(int i)
+	{		
+		Console.WriteLine($"Processing {i}");
+		await Task.Delay(TimeSpan.FromSeconds(5));
+		return i * 5;
+	}
+}
